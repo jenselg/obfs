ad82415b838e74e7af1b19ade2c69481ab3ba35b
Jensel Gatchalian <jensel.gatchalian@gmail.com>
2019-09-05 15:33:08 -0700

    Update code comments

--- obj.js
+++ obj.js
@@ -9,8 +9,8 @@
 
   MIT License
 
-  Copyright (c) 2019 Jensel Gatchalian <jensel.gatchalian@gmail.com>
-
+  Copyright (c) 2019 Jensel Gatchalian
+  
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
